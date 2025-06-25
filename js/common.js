dc749969f117c8f46da9fd4d9d86bd0c11063edb
timcho19 <timcho4589@gmail.com>
2025-06-25 16:16:11 +0900

    sidebar 구현 html css script

---
+++ js/common.js
@@ -0,0 +1,12 @@
+$('.sidebar_menu').on('click', function() {
+   $('.side_bar').stop().toggleClass('hide-menu');
+});
+
+$('.top_btn').on('click', function() {
+  $('html, body').animate({ scrollTop: 0 }, 200);
+
+});
+
+
+
+
