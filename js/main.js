16ad65f5d16d4c6d827b65efb2f1ded6cc7cd374
timcho19 <timcho4589@gmail.com>
2025-06-26 17:21:00 +0900

    메인 자동슬라이드

---
+++ js/main.js
@@ -0,0 +1,22 @@
+//메인섹션 슬ㄹ이드
+const mslides = $('.mslides-container .mslide');
+let current = 0;
+const mslideCount = mslides.length;
+const interval = 5000; // 5초
+
+function showSlide(idx) {
+  mslides.removeClass('active').eq(idx).addClass('active');
+}
+
+function nextSlide() {
+  current = (current + 1) % mslideCount;
+  showSlide(current);
+}
+
+let timer = setInterval(nextSlide, interval);
+
+
+// $('.slides-container, .main-title-area').hover(
+//   function() { clearInterval(timer); },
+//   function() { timer = setInterval(nextSlide, interval); }
+// );
