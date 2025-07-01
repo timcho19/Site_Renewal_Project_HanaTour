64978697d412451246d2e994cfd6045da55d17d3
likerang <arang9314@naver.com>
2025-07-01 15:46:26 +0900

    피드백 + guest radio 버튼 수정

--- js/guest.js
+++ js/guest.js
@@ -1,27 +1,30 @@
 const flightHotel = $('.radio_btn #flight-hotel');
 const guestInfo = $('.guest-info-search');
-const formInput = $('.form-floating-custom');
-const guestName = $('.guest_name');
+const formInput = $('.guest-info-search .form-floating-custom');
+const guestPhone = $('.guest_phone');
 const guestEmail = $('.guest_email');
 
 
-
-flightHotel.change(function(){
-  if($(this).val() === 'on'){
-    console.log($(this).val())
-    guestInfo.hide();
-  }else{
+/* - - 비회원 카테고리별 조회 - - */
+$('input[name="package"]').change(function () {
+  const selected = $('input[id="package"]:checked').val();
+  console.log(selected);
+  if (selected === 'package') {
     guestInfo.show();
+  } else {
+    guestInfo.hide();
   }
 });
 
-$('input[name="guest"]').change(function(){
-  $('.form-floating-custom').removeClass('active');
 
-      if (this.id === 'guest_phone') {
-      guestName.addClass('active');
+
+$('input[name="guest"]').change(function(){
+  let checked = $(this).val();
+  console.log(checked);
+  formInput.removeClass('active');
+  if (checked === 'email') {
+    guestEmail.addClass('active');
     } else {
-      guestEmail.addClass('active');
-      guestName.css({display:'none'});
+      guestPhone.addClass('active');
     }
 });
--- js/main.js
+++ js/main.js
@@ -1,49 +1,54 @@
 let slideWrapper = $('.story_wrapper'),
-                slides = slideWrapper.find('.stories'),
-                slide = slides.find('.story'),
-                currentIdx = 0,
-                slideCount = slide.length,
-                slideWidth = 1142,
-                slideMargin = 90,
-                moveAmt = slideWidth + slideMargin,
-                halfAmt = slideWidth/3;
-                prevBtn = slideWrapper.find('.controls .prev_btn'),
-                nextBtn = slideWrapper.find('.controls .next_btn');
+    slides = slideWrapper.find('.stories'),
+    slide = slides.find('.story'),
+    currentIdx = 0,
+    slideCount = slide.length,
+    slideWidth = 1142,
+    slideMargin = 90,
+    moveAmt = slideWidth + slideMargin,
+    halfAmt = slideWidth/3;
+    prevBtn = slideWrapper.find('.controls .prev_btn'),
+    nextBtn = slideWrapper.find('.controls .next_btn');
 
 
 
-    slides.prepend(slide.clone());
-    slide.clone().appendTo(slides);
+  slides.prepend(slide.clone());
+  slide.clone().appendTo(slides);
 
-    let allSlide = slides.find('.story');
-    let totalCount = allSlide.length;
+  let allSlide = slides.find('.story');
+  let totalCount = allSlide.length;
 
-    allSlide.each(function (idx) {
-        $(this).css({ left: idx * moveAmt + halfAmt + 'px' });
+  allSlide.each(function (idx) {
+      $(this).css({ left: idx * moveAmt + halfAmt + 'px' });
+    });
+  
+    slides.css('width', moveAmt * totalCount + 'px');
+
+
+    let startIdx = slideCount; 
+    slides.css('left', -startIdx * moveAmt + 'px');
+    currentIdx = startIdx;
+
+    function moveSlide(num) {
+      slides.stop().animate({ left: -num * moveAmt + 'px' }, 500, function () {
+        if (num >= totalCount - slideCount || num < slideCount) {
+          slides.css('left', -startIdx * moveAmt + 'px');
+          currentIdx = startIdx;
+        }
       });
-    
-      slides.css('width', moveAmt * totalCount + 'px');
+    }
+
+  nextBtn.click(function(){
+      moveSlide(++currentIdx);
+  });
+  prevBtn.click(function(){
+      moveSlide(--currentIdx);
+  });
 
 
-      let startIdx = slideCount; 
-      slides.css('left', -startIdx * moveAmt + 'px');
-      currentIdx = startIdx;
 
-      function moveSlide(num) {
-        slides.stop().animate({ left: -num * moveAmt + 'px' }, 500, function () {
-          if (num >= totalCount - slideCount || num < slideCount) {
-            slides.css('left', -startIdx * moveAmt + 'px');
-            currentIdx = startIdx;
-          }
-        });
-      }
 
-    nextBtn.click(function(){
-        moveSlide(++currentIdx);
-    });
-    prevBtn.click(function(){
-        moveSlide(--currentIdx);
-    });
+
 //메인섹션 슬ㄹ이드
 const mslides = $('.mslides-container .mslide');
 let current = 0;
@@ -168,7 +173,7 @@ $('.slider-wrapper .contents_next_btn').click(function(){
   contentSlide(1);
 });
 }
-export default slideshow;
+//export default slideshow;
 
 
     
