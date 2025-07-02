f3b0dc3d13a729927f69125ffbae3febdd5b1a3a
hiyokiki <bada8317@gmail.com>
2025-07-02 15:16:57 +0900

    qna

--- js/faq.js
+++ js/faq.js
@@ -5,15 +5,3 @@ $('.all_menu_btn').on('click', function(e) {
 });
 
 
-
-  document.querySelectorAll('.accordion-button').forEach(button => {
-    button.addEventListener('click', function (e) {
-      const target = document.querySelector(this.dataset.bsTarget);
-      if (target.classList.contains('show')) {
-        // 이미 열려 있으면 강제로 닫기
-        const collapse = bootstrap.Collapse.getInstance(target);
-        collapse.hide();
-        e.preventDefault(); // 기본 Bootstrap 열기 방지
-      }
-    });
-  });
