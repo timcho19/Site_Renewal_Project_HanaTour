9aedac6d4d3cef92e193979b312d6397c66a2413
hiyokiki <bada8317@gmail.com>
2025-07-07 17:05:52 +0900

    어어어

--- js/main.js
+++ js/main.js
@@ -170,7 +170,7 @@ $(window).scroll(function(){
 
 /* 날씨/무한슬라이드 */
 
-const cities = ['Rome,IT', 'New York', 'japan', 'thailand', 'shanghai', 'singapore', 'paris', 'australia', 'australia', 'Bern,CH' ];
+const cities = ['Rome,IT', 'New York', 'japan', 'thailand', 'shanghai', 'singapore', 'paris', 'australia', 'Agra,IN', 'Bern,CH' ];
 let loadedCount = 0;
 
 const getWeather = (cityName) => {
