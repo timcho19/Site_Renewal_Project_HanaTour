6ad2ed05bdde564594f0986dffc283aeda766340
hiyokiki <bada8317@gmail.com>
2025-07-07 13:05:22 +0900

    수정

--- js/banner.js
+++ js/banner.js
@@ -6,8 +6,18 @@ const bannerslideshow = function(target) {
     const totalSlides = $slides.length;
   
     const $indicator = $container.find('#slideIndicator');
-    const $dots = $container.find('.dot');
+    const $dotsContainer = $container.find('#dotsIndicator');
     let autoplay;
+  
+
+    $dotsContainer.empty();
+    for (let i = 0; i < totalSlides; i++) {
+      const $dot = $('<span class="dot"></span>');
+      if (i === 0) $dot.addClass('active');
+      $dotsContainer.append($dot);
+    }
+  
+    const $dots = $dotsContainer.find('.dot'); 
   
     function updateSlider() {
       const translateX = -currentSlide * 100;
@@ -34,18 +44,18 @@ const bannerslideshow = function(target) {
       clearInterval(autoplay);
     }
   
-
     startAutoplay();
   
     $container
       .on('mouseenter', stopAutoplay)
       .on('mouseleave', startAutoplay);
   
-
     $dots.each(function(index) {
       $(this).on('click', function() {
         goToSlide(index);
       });
     });
   };
-export default  bannerslideshow;
+  
+  export default bannerslideshow;
+  
