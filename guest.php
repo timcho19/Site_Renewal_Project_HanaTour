6dc921766c82ae541ca6ab7d0db0ce285090ac31
likerang <arang9314@naver.com>
2025-07-09 17:00:35 +0900

    main css 일괄 추가

--- guest.php
+++ guest.php
@@ -1,11 +1,12 @@
 <?php
   require('inc/functions.php');
 
+  $mainCss = '<link rel="stylesheet" href="css/main.css">';
   $loginCss = '<link rel="stylesheet" href="css/login.css">';
   $guestCss = '<link rel="stylesheet" href="css/guest.css">';
   $loginScript = '<script src="js/login.js"></script>';
   $guestScript = '<script src="js/guest.js"></script>';
 
 
-  sub_view('guest', '하나투어 비회원 조회', $loginCss, $guestCss, '', $loginScript, $guestScript, '');
+  sub_view('guest', '하나투어 비회원 조회', $mainCss ,$loginCss, $guestCss, $loginScript, $guestScript, '');
 ?>
--- login.php
+++ login.php
@@ -1,10 +1,12 @@
 <?php
   require('inc/functions.php');
 
+  $mainCss = '<link rel="stylesheet" href="css/main.css">';
   $loginCss = '<link rel="stylesheet" href="css/login.css">';
   $loginScript = '<script src="js/login.js"></script>';
+  $bootstrap = '<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>';
 
 
 
-  sub_view('login', '하나투어 로그인', $loginCss, '', '', $loginScript, '', '');
+  sub_view('login', '하나투어 로그인', $mainCss, $loginCss, '', $loginScript, $bootstrap, '');
 ?>
