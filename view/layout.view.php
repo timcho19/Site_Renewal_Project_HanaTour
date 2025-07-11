fe9b5b66d7dd1cb65babadb9eb4c71286567c427
hiyokiki <bada8317@gmail.com>
2025-07-11 11:40:41 +0900

    like버튼이름들

--- view/layout.view.php
+++ view/layout.view.php
@@ -179,7 +179,7 @@
                   <img src="image/aside_login.png" alt="로그인"> 로그인
                 </a>
               <?php endif; ?>
-              <button class="aside_close"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
+              <button  aria-label="햄버거메뉴닫기버튼"   class="aside_close"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-x-lg" viewBox="0 0 16 16">
                 <path d="M2.146 2.854a.5.5 0 1 1 .708-.708L8 7.293l5.146-5.147a.5.5 0 0 1 .708.708L8.707 8l5.147 5.146a.5.5 0 0 1-.708.708L8 8.707l-5.146 5.147a.5.5 0 0 1-.708-.708L7.293 8z"/>
               </svg></button>
             </div>
